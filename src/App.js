46b4ac9308c8d457b89537f443272421d93802cb
cesar.olivera <cesar.olivera00@gmail.com>
2022-04-17 18:40:36 -0300

    Add TopNavBar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+// Components
+import TopNavBar from './components/TopNavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TopNavBar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/TopNavBar.js
@@ -0,0 +1,18 @@
+import React, {Component} from "react";
+
+class TopNavBar extends Component {
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                    <li>Item 4</li>
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export default TopNavBar;
